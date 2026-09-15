e3814144c46a7e26276bb06a80aecc3cc08e6a16
noorsaba <noorsaba.safi@etu-univ.fr>
2026-09-15 22:17:42 +0200

    Add TD1 Java exercises

---
+++ Bike.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+
+public class Bike {
+
+    private static final char[] DEFAULT_BRAND = {'B', 'i', 'k', 'e'};
+    private static final Color DEFAULT_COLOR = Color.BLACK;
+    private static final int DEFAULT_SIZE = 26;
+
+    private final char[] brand = DEFAULT_BRAND;
+    private Color color = DEFAULT_COLOR;
+    private int speed = 0;
+    private final int size = DEFAULT_SIZE;
+
+    public void print() {
+        System.out.println("[" + new String(brand) + " - " + size + "\" - " + color + " - (" + speed + "km/h)]");
+    }
+
+  public void accelerate ()
+  {
+    this.speed++ ; 
+  }
+
+  public void brake()
+  {
+    this.speed--; 
+  }
+ 
+ public void repaint(Color c) {
+    this.color = c ; 
+}
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bike bike = new Bike();
+        bike.print();
+    }
+    
+} 
+
