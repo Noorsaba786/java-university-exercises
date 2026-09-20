cebfac3a9852dcf0770532d8935ce1a659f54643
noorsaba <noorsabasafi007@gmail.com>
2026-09-20 12:28:19 +0200

    Door states exercices in java

---
+++ Td1/Door.java
@@ -0,0 +1,71 @@
+/* public class Door {
+    private boolean isOpen;
+    private boolean isLocked;
+
+    public void close() {
+        isOpen = false;
+    }
+
+    public void open() {
+        if (isLocked) {
+            return;
+        }
+        isOpen = true;
+    }
+
+    public void unlock() {
+        isLocked = false;
+    }
+
+    public void lock() {
+        if (isOpen) {
+            return;
+        }
+        isLocked = true;
+    }
+
+    public void print() {
+        String doorState = isOpen ? "open " : "closed";
+        String lockState = isLocked ? "locked" : "unlocked";
+        System.out.println("the door is " + doorState + " ," + lockState);
+    }
+
+} */
+
+public class Door {
+    private boolean isOpen;
+    private boolean isLocked;
+
+    public Door() {
+        this.isOpen = false; // by default loock
+        this.isLocked = false; // by defalut open
+    }
+
+    public void close() {
+        this.isOpen = false;
+
+    }
+
+    public void open() {
+        if (isLocked == false) {
+            this.isOpen = true;
+        }
+    }
+
+    public void lock() {
+        if (isOpen == false) {
+            this.isLocked = true;
+        }
+
+    }
+
+    public void unlock() {
+        this.isLocked = false;
+    }
+
+    public void printStates() {
+        String openState = isOpen ? " open" : " close";
+        String lockState = isLocked ? " locked" : "not locked";
+        System.out.println("the door is " + openState + " and " + lockState);
+    }
+}
---
+++ Td1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Door d = new Door();
+
+        d.lock();
+        d.printStates();
+
+    }
+}
