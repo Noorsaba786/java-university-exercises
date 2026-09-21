5f5697e843ef9b162a792e1e3d7157dccd589bc7
noorsaba <noorsabasafi007@gmail.com>
2026-09-21 08:52:49 +0200

    Exo 1 Tp 'a classe minimale'

--- Tp1/Bike.java
+++ Tp1/Bike.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
 
+public static final String DEFAULT_BRAND = "Electra";
+public static final Color DEFAULT_COLOR = Color.RED;
+public static final int DEFAULT_SIZE = 26;
+
 public class Bike {
 
     private final String brand;
@@ -12,6 +16,24 @@ public class Bike {
         this.color = color;
         this.size = size;
         this.speed = 0;
+    }
+
+    // speed stays in [0, 60]
+    public void accelerate() {
+        if (speed < 60) {
+            speed++;
+        }
+    }
+
+    public void brake() {
+        if (speed > 0) {
+            speed--;
+        }
+    }
+
+    // repaint ONLY to the default color
+    public void repaint() {
+        color = DEFAULT_COLOR;
     }
 
     public void print() {
--- Tp1/Main.java
+++ Tp1/Main.java
@@ -2,7 +2,10 @@ import java.awt.Color;
 
 public class Main {
     public static void main(String[] args) {
-        Bike b1 = new Bike("Honda ", Color.RED, 26);
+        Bike b1 = new Bike(Bike.DEFAULT_BRAND, Bike.DEFAULT_COLOR, Bike.DEFAULT_SIZE);
+        Bike b2 = new Bike("Btwin", Color.BLUE, 28);
         b1.print();
+        b2.print();
+
     }
 }
